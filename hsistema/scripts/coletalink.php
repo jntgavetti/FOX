2b01d36ad26736f1afa722fbee2a7ceb09f7398f
jntgavetti <59202444+jntgavetti@users.noreply.github.com>
2021-04-12 20:40:05 -0300

    Testes no linktest novo

--- hsistema/scripts/coletalink.php
+++ hsistema/scripts/coletalink.php
@@ -1,39 +1,52 @@
-<?php 
-    
-    $handle_interfaces = fopen('interfaces', 'r');
-    $i = 0;
-    $interface = new stdClass;
-    $new_int = false;
+DEV_EXT=`cat /opt/hsistema/config/cliente.txt | grep int_prov$enum | cut -d '=' -f2`
+DEV_TY=`cat /opt/hsistema/config/cliente.txt | grep dis_prov$enum | cut -d '=' -f 2`
+OPERAD=`cat /opt/hsistema/config/cliente.txt | grep oper_prov$enum | cut -d '=' -f 2`
+LINK_S=`cat /opt/hsistema/config/cliente.txt | grep def_prov$enum | cut -d '=' -f 2`
+PROVE=`cat /opt/hsistema/config/cliente.txt | grep def_provedor$enum | cut -d '=' -f 2`
+ADDR=`cat /opt/hsistema/config/$DEV_EXT.conf | grep address | cut -d ' ' -f 2`
+GATE=`cat /opt/hsistema/config/$DEV_EXT.conf | grep gateway | cut -d ' ' -f 2`
 
-    while(!feof($handle_interfaces)){
-        $line = fgets($handle_interfaces);
+echo "$DEV_EXT" > /opt/hsistema/links/w"$enum"dispositivo
+echo "$OPERAD" > /opt/hsistema/links/w"$enum"nome
+echo "$LINK_S" > /opt/hsistema/links/w"$enum"Prioridade
+echo "$ADDR" > /opt/hsistema/links/w"$enum"ipPlaca
+echo "$GATE" > /opt/hsistema/links/w"$enum"Gateway
 
-        $regex_new_interface = "/^allow-hotplug |^auto [^lo\r\n]/i";
-        $regex_iface = "/^iface [^lo\r\n]/i";
-        $regex_name_interface = "/eth[0-9:]+|(^tap.+$|^tap$)/";
-       
-        
-        if(preg_match($regex_iface, $line)){
-            continue;
-        }
 
-        if(preg_match($regex_new_interface, $line)){
-            preg_match($regex_name_interface, $line, $int);
-            $nome = $int[0];
-            $interface -> $nome = [];
-            $new_int = true;
-            $i = 0;
-            continue;
-        }
-        
-        if($new_int){
-            $interface -> $nome[$i] = $line;
-            $i++;
-        }
-        
-        
+<?php
+
+$handle_interfaces = fopen('interfaces', 'r');
+$i = 0;
+$interface = new stdClass;
+$new_int = false;
+
+while (!feof($handle_interfaces)) {
+    $line = fgets($handle_interfaces);
+
+    $regex_new_interface = "/^allow-hotplug |^auto [^lo\r\n]/i";
+    $regex_iface = "/^iface [^lo\r\n]/i";
+    $regex_name_interface = "/eth[0-9:]+|(^tap.+$|^tap$)/";
+
+
+    if (preg_match($regex_iface, $line)) {
+        continue;
     }
-    
-    
-    var_dump($interface);
+
+    if (preg_match($regex_new_interface, $line)) {
+        preg_match($regex_name_interface, $line, $int);
+        $nome = $int[0];
+        $interface->$nome = [];
+        $new_int = true;
+        $i = 0;
+        continue;
+    }
+
+    if ($new_int) {
+        $interface->$nome[$i] = $line;
+        $i++;
+    }
+}
+
+
+var_dump($interface);
 ?>
--- hsistema/scripts/linktest.sh
+++ hsistema/scripts/linktest.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 PATH="/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin"
 export LANG=pt_BR.UTF-8
-ONTEM=`date +%Y"-"%m"-"%d -d "1 days ago"`
-DIA=`date +%Y"-"%m"-"%d`
-DIAHORA=`date +%d"/"%m"/"%Y"-"%H":"%M":"%S`
+ONTEM=$(date +%Y"-"%m"-"%d -d "1 days ago")
+DIA=$(date +%Y"-"%m"-"%d)
+DIAHORA=$(date +%d"/"%m"/"%Y"-"%H":"%M":"%S)
 
 if [ "$1" != " " ]; then
         if [ "$1" = "versao" ]; then
-            echo -e v1.00 32
-            exit
-          else
-            echo
+                echo -e v1.00 32
+                exit
+        else
+                echo
         fi
 fi
 
 if [ "$1" != " " ]; then
         if [ "$1" = "show" ]; then
-            echo
-          else
-            exec 1>> /opt/hsistema/log/$DIA-linkteste.log 2>&1
+                echo
+        else
+                exec 1>>/opt/hsistema/log/$DIA-linkteste.log 2>&1
         fi
 fi
 
@@ -31,127 +31,105 @@ IP="/sbin/ip"
 NP="/usr/bin/nmap"
 IS="/usr/sbin/ipsec"
 
-LINK1=`cat /opt/hsistema/config/cliente.txt | grep host_12 | cut -d ' ' -f 3`
+SAFE_ADDR="/opt/hsistema/config/enderecos_seguros"
+LINKS_EXT=$(cat /opt/hsistema/config/cliente.txt | grep links_externos | cut -d '=' -f2)
 
+teste() {
+        $1 = INTERFACE
+        $2 = PORTA
+        $3 = DESTINO
+        $4 = QTD
 
-link_fail () 
-                LSTATUS=down
-                IP_EXTERNO=`cat /opt/hsistema/config/ip_$DEV_EXT.txt`
-                echo "$IP_EXTERNO" > /opt/hsistema/links/w"$enum"ipExterno
-                echo "down" > /opt/hsistema/links/w"$enum"status
-                echo "down" > /opt/hsistema/config/$DEV_EXT.status
-                ver_hist
+        report_ping="$(ping -p 80 -c 3 -q -I eth0 192.168.0.1)"
+        average="$(echo $report_ping | grep rtt | cut -d '=' -f2 | cut -d '/' -f 3)"
+        packet_loss="$(echo $report_ping | grep -Po '[0-9]+%')"
+}
+
+link_fail() {
+        LSTATUS=down
+        IP_EXTERNO=$(cat /opt/hsistema/config/ip_$INTERFACE.txt)
+        echo "$IP_EXTERNO" >/opt/hsistema/links/w"$enum"ipExterno
+        echo "down" >/opt/hsistema/links/w"$enum"status
+        echo "down" >/opt/hsistema/config/$INTERFACE.status
+        ver_hist
 
 }
 
-link_ok () {
-                LSTATUS=up
-                IP_EXTERNO=`lynx -connect_timeout=5 -dump https://www.h2info.com.br/suporte/ | grep "Seu IP" | cut -d ' ' -f4`
-                echo "$IP_EXTERNO" > /opt/hsistema/links/w"$enum"ipExterno
-                echo "$IP_EXTERNO" > /opt/hsistema/config/ip_$DEV_EXT.txt
-                echo "up" > /opt/hsistema/links/w"$enum"status
-                echo "up" > /opt/hsistema/config/$DEV_EXT.status
-                ver_hist
+link_ok() {
+        LSTATUS=up
+        IP_EXTERNO=$(lynx -connect_timeout=5 -dump https://www.h2info.com.br/suporte/ | grep "Seu IP" | cut -d ' ' -f4)
+        echo "$IP_EXTERNO" >/opt/hsistema/links/w"$enum"ipExterno
+        echo "$IP_EXTERNO" >/opt/hsistema/config/ip_$INTERFACE.txt
+        echo "up" >/opt/hsistema/links/w"$enum"status
+        echo "up" >/opt/hsistema/config/$INTERFACE.status
+        ver_hist
 }
 
-ver_hist () {
-        LSTATUSOLD=`cat /opt/hsistema/links/w"$enum"status.old`
+ver_hist() {
+        LSTATUSOLD=$(cat /opt/hsistema/links/w"$enum"status.old)
         if [ $LSTATUSOLD = $LSTATUS ]; then
                 echo link inalterado
         else
-                echo "$DIAHORA - $DEV_EXT - $LSTATUS" >> /opt/hsistema/links/$DEV_EXT-status.hist
-                echo "$DIAHORA - $DEV_EXT - $LSTATUS" > /opt/hsistema/links/w"$enum"status.hsite
-                echo "$LSTATUS" > /opt/hsistema/links/w"$enum"status.old
+                echo "$DIAHORA - $INTERFACE - $LSTATUS" >>/opt/hsistema/links/$INTERFACE-status.hist
+                echo "$DIAHORA - $INTERFACE - $LSTATUS" >/opt/hsistema/links/w"$enum"status.hsite
+                echo "$LSTATUS" >/opt/hsistema/links/w"$enum"status.old
         fi
 }
 
-
 # Teste de link
 
-enum=0
-while [ $enum -lt $QLINKEXT ]; do
-        enum=$(($enum+1))
-        STATUS=`cat /opt/hsistema/config/cliente.txt | grep sta_prov$enum | cut -d '=' -f 2`
-        if [ "$STATUS" != "" ]; then
-                if [ $STATUS = "ativo" ]; then
-                        DEV_EXT=`cat /opt/hsistema/config/cliente.txt | grep int_prov$enum | cut -d '=' -f2`
-                        DEV_TY=`cat /opt/hsistema/config/cliente.txt | grep dis_prov$enum | cut -d '=' -f 2`
-                        OPERAD=`cat /opt/hsistema/config/cliente.txt | grep oper_prov$enum | cut -d '=' -f 2`
-                        LINK_S=`cat /opt/hsistema/config/cliente.txt | grep def_prov$enum | cut -d '=' -f 2`
-                        PROVE=`cat /opt/hsistema/config/cliente.txt | grep def_provedor$enum | cut -d '=' -f 2`
-			ADDR=`cat /opt/hsistema/config/$DEV_EXT.conf | grep address | cut -d ' ' -f 2`
-                        GATE=`cat /opt/hsistema/config/$DEV_EXT.conf | grep gateway | cut -d ' ' -f 2`
-			
+cont=0
+while [ $cont -lt $LINKS_EXT ]; do
+
+        cont=$(($cont + 1))
+
+        STATUS_LINK=$(cat /opt/hsistema/config/cliente.txt | grep sta_prov$enum | cut -d '=' -f 2)
+
+        if [ "$STATUS_LINK" != "" ]; then
+
+                if [ $STATUS_LINK = "ativo" ]; then
+
+                        INTERFACE=$(cat /opt/hsistema/config/cliente.txt | grep int_prov$enum | cut -d '=' -f2)
+                        DEV_TYPE=$(cat /opt/hsistema/config/cliente.txt | grep dis_prov$enum | cut -d '=' -f 2)
+                        OPERAD=$(cat /opt/hsistema/config/cliente.txt | grep oper_prov$enum | cut -d '=' -f 2)
+                        LINK_S=$(cat /opt/hsistema/config/cliente.txt | grep def_prov$enum | cut -d '=' -f 2)
+                        PROVE=$(cat /opt/hsistema/config/cliente.txt | grep def_provedor$enum | cut -d '=' -f 2)
+                        ADDR=$(cat /opt/hsistema/config/$INTERFACE.conf | grep address | cut -d ' ' -f 2)
+                        GATE=$(cat /opt/hsistema/config/$INTERFACE.conf | grep gateway | cut -d ' ' -f 2)
+
+
                         
-			echo "$DEV_EXT" > /opt/hsistema/links/w"$enum"dispositivo
-                        echo "$OPERAD" > /opt/hsistema/links/w"$enum"nome
-                        echo "$LINK_S" > /opt/hsistema/links/w"$enum"Prioridade
-                        echo "$ADDR" > /opt/hsistema/links/w"$enum"ipPlaca
-			echo "$GATE" > /opt/hsistema/links/w"$enum"Gateway
-			
-                        $RT -n | grep $DEV_EXT
+
+                        $RT -n | grep $INTERFACE
                         if [ $? -eq 1 ]; then
                                 link_fail
                         else
-                                if [ $DEV_TY = dev ]; then
-                                        $RT add -net $LINK5 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT add -net $LINK1 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT add -net $LINK2 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT add -net $LINK3 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT add -net $LINK4 netmask 255.255.255.255 dev $DEV_EXT
-                                        $IP route flush cache
-                                else
-                                        $RT add -net $LINK5 netmask 255.255.255.255 gw $GATE
-                                        $RT add -net $LINK1 netmask 255.255.255.255 gw $GATE
-                                        $RT add -net $LINK2 netmask 255.255.255.255 gw $GATE
-                                        $RT add -net $LINK3 netmask 255.255.255.255 gw $GATE
-                                        $RT add -net $LINK4 netmask 255.255.255.255 gw $GATE
-                                        $IP route flush cache
-                                fi
-                                {
-                                ping -p 80 -c 3 -I $DEV_EXT $LINK2 | grep ttl
-                                if [ $? -eq 1 ]; then
-                                        ping -p 80 -c 3 -I $DEV_EXT $LINK3 | grep ttl
-                                        if [ $? -eq 1 ]; then
-                                                link_fail
-                                        else
-                                                ping -p 443 -c 3 -I $DEV_EXT $LINK3 | grep ttl
-                                                if [ $? -eq 1 ]; then
-                                                        link_fail
-                                                else
-                                                        link_ok
-                                                fi
-                                        fi
-                                else
-                                        ping -p 443 -c 3 -I $DEV_EXT $LINK2 | grep ttl
-                                        if [ $? -eq 1 ]; then
-                                                link_fail
-                                        else
-                                                link_ok
-                                        fi
-                                fi
-                                }
+                                cont_addr=0
 
-                                if [ $DEV_TY = "dev" ]; then
-                                        $RT del -net $LINK5 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT del -net $LINK1 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT del -net $LINK2 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT del -net $LINK3 netmask 255.255.255.255 dev $DEV_EXT
-                                        $RT del -net $LINK4 netmask 255.255.255.255 dev $DEV_EXT
-                                        $IP route flush cache
-                                else
-                                        $RT del -net $LINK5 netmask 255.255.255.255 gw $GATE
-                                        $RT del -net $LINK1 netmask 255.255.255.255 gw $GATE
-                                        $RT del -net $LINK2 netmask 255.255.255.255 gw $GATE
-                                        $RT del -net $LINK3 netmask 255.255.255.255 gw $GATE
-                                        $RT del -net $LINK4 netmask 255.255.255.255 gw $GATE
-                                        $IP route flush cache
-                                fi
+                                while read addr; do
+                                        $DNS_ADDR="$addr" | cut -d ';' -f 1
+                                        $IP_ADDR="$addr" | cut -d ';' -f 2
+
+                                        if [ $DEV_TYPE = dev ]; then
+                                                $RT add -net $DNS_ADDR netmask 255.255.255.255 gw $GATE dev $INTERFACE
+                                                $RT add -net $IP_ADDR netmask 255.255.255.255 gw $GATE dev $INTERFACE
+                                                $IP route flush cache
+                                        else
+                                                $RT add -net $LINK5 netmask 255.255.255.255 gw $GATE
+                                                $IP route flush cache
+                                        fi
+
+                                done \
+                                        < \
+                                        \
+                                        \
+                                        $SAFE_ADDR
+
                         fi
+
                 fi
-         fi
+        fi
 done
 
-        DIAHORA=`date +%d"/"%m"/"%Y"-"%H":"%M":"%S`
-        echo -e "\n\t$DIAHORA - Fim da Rotina Link teste \n"
-
+DIAHORA=$(date +%d"/"%m"/"%Y"-"%H":"%M":"%S)
+echo -e "\n\t$DIAHORA - Fim da Rotina Link teste \n"
